0a26097ef17fc8fa3f89c283edb546c80164f484
e1 <demonfisk@gmail.com>
2021-04-11 21:05:17 +0300

    added BASE_URL env variable

--- src/main/java/com/project/BP/services/RegistrationService.java
+++ src/main/java/com/project/BP/services/RegistrationService.java
@@ -69,7 +69,7 @@ public class RegistrationService {
                 )
         );
 
-        String link = "http://localhost:8080/api/v1/registration/confirm?token=" + token;
+        String link = System.getenv("BASE_URL") + "/api/v1/registration/confirm?token=" + token;
         emailSender.send(
                 request.getEmail(),
                 buildEmail(request.getFirstName(), link));
